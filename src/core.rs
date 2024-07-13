56d973cd519b47a54136777e93f843465b032990
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-07-14 00:17:54 +0200

    added workflow

--- src/core.rs
+++ src/core.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 
 pub fn load_icon() -> Result<egui::IconData, Box<dyn Error>> {
 	let (icon_rgba, icon_width, icon_height) = {
-		let icon = include_bytes!("../assets/mabo.ico");
+		let icon = include_bytes!("../assets/kettle.ico");
 		let image = image::load_from_memory(icon)?;
 		let rgba = image.clone().into_rgba8().to_vec();
 		let (width, height) = image.dimensions();
